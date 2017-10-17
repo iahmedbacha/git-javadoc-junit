2eeac9976c2620c3dc4eb39495e2d08fcc0675af
iahmedbacha <fi_ahmed_bacha@esi.dz>
2017-10-17 17:01:18 +0100

    Créer fct tri dans VectorHelper

--- src/VectorHelper.java
+++ src/VectorHelper.java
@@ -1,2 +1,22 @@
 public class VectorHelper {
+
+    public static int[] tri (int[] tab) {
+        int permut_cpt;
+        boolean continu = true;
+        while (continu) {
+            permut_cpt = 0;
+            for (int i=0;i<tab.length-1;i++) {
+                if (tab[i]>tab[i+1]) {
+                    int temp=tab[i];
+                    tab[i]=tab[i + 1];
+                    tab[i+1]=temp;
+                    permut_cpt++;
+                }
+            }
+            if (permut_cpt==0) {
+                continu = false;
+            }
+        }
+        return tab;
+    }
 }
